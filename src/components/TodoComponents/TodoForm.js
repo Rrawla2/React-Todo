8a2286c2acc3c78a95dc7708ece2839f85e26c27
Kristi Gribble <kgribble4083@gmail.com>
2020-01-15 18:33:00 -0600

    Added basic classes and variables, basic div to display

---
+++ src/components/TodoComponents/TodoForm.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+class TodoForm extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            name: ""
+        }
+    }
+
+    render() {
+        return (
+            <div>Todo Form</div>
+        )
+    }
+}
+
+export default TodoForm;
