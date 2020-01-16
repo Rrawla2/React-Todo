802f6d5508936878c3b0f098a07c0c7c5ab30006
Kristi Gribble <kgribble4083@gmail.com>
2020-01-15 20:25:54 -0600

    Added Clear Completed button

--- src/components/TodoComponents/TodoList.js
+++ src/components/TodoComponents/TodoList.js
@@ -1,10 +1,15 @@
 // your components will all go in this `component` directory.
 // feel free to change this component.js into TodoList.js
 import React from "react";
+import Todo from "./Todo";
 
-const TodoList = () => {
+const TodoList = (props) => {
     return (
-        <div>Todo List</div>
+        <div>Todo List
+        
+        <button>Clear Completed</button>
+        </div>
+
     )
 }
 
